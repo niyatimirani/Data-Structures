8719d29a5f495dcf7ccba1d5f8f174b627200220
Niyati Mirani <122291106+niyatimirani@users.noreply.github.com>
2025-07-16 12:18:59 +0530

    Create Array_second_largest.java

---
+++ Array_second_largest.java
@@ -0,0 +1,26 @@
+// Second Largest 
+
+import java.util.*;
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        if(arr.length < 2)
+           return -1;
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        
+        for(int num : arr)
+        {
+            if(num > largest)
+            {
+                secondLargest=largest;
+                largest=num;
+            }else if(num >secondLargest && num != largest)
+            {
+                secondLargest = num;
+            }
+        }
+        
+        return (secondLargest == Integer.MIN_VALUE) ? -1 : secondLargest;
+        
+    }
+}
